bb7fc44ebcb902f7c78ccfa830ae7247d287b9ff
starbuckshiker <u1304954@umail.utah.edu>
2020-09-25 14:53:08 -0600

    emp add

--- employee.py
+++ employee.py
@@ -21,6 +21,13 @@ class Employee:
         self.pay = int(self.pay * self.raise_amt)
 
 
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text:
+        else:
+            return 'Bad Response'
+            
 emp_1 = Employee('Corer', 'Schafer', 50000)
 emp_2 = Employee( 'Test', 'User', 60000)
 
