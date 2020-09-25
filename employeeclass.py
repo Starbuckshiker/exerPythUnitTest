ac7ff9b647a0ab57ca97a09a366389ccc614ae4a
starbuckshiker <u1304954@umail.utah.edu>
2020-09-25 13:40:08 -0600

    raise amt fix

--- employeeclass.py
+++ employeeclass.py
@@ -1,6 +1,7 @@
 class Employee:
-    
-    raise_amount = 1.05
+    """A sample Employee Class"""
+
+    raise_amt = 1.05
     
     def __init__(self, first, last, pay):
         self.first = first
@@ -9,15 +10,15 @@ class Employee:
        
     @property   
     def email(self):
-        return '{} {}@email.com'.format(self.first, self.last)
+        return '{}.{}@email.com'.format(self.first, self.last)
     
     @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    
-    def apply_raises(self)
-        self.pay = int(self.pay * serlf.raise_amt)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
 
 
 emp_1 = Employee('Corer', 'Schafer', 50000)
@@ -26,8 +27,6 @@ emp_2 = Employee( 'Test', 'User', 60000)
 #This allows you to adjust the instance of emp_1 instead of the class "Employee"
 emp_1.raise_amount = 1.05
         
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+print(Employee.raise_amt)
 
-print(Employee.num_of_emps)
+
